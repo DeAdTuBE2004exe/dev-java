b0a349f143ade5a422d869db9ecedcd81d12c045
DeAdTuBE2004exe <bharatkulkarni.0000@gmail.com>
2025-04-09 09:46:50 +0530

    adding chain file to repo

---
+++ chain.java
@@ -0,0 +1,13 @@
+public class chain {
+    chain(){
+        this(10);
+        System.out.println("default called");
+    }
+    chain (int x){
+        System.out.println("para1 called");
+    }
+    public static void main(String args[]){
+        chain obj = new chain();
+        obj = null;
+    }
+}
