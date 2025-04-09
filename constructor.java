be3d5c08ba0c2dd950b733503ffb1e2d42ee5040
DeAdTuBE2004exe <bharatkulkarni.0000@gmail.com>
2025-04-09 09:40:30 +0530

    adding java files to repo

---
+++ constructor.java
@@ -0,0 +1,13 @@
+public class constructor {
+    constructor(){
+        System.out.println("default constructor called");
+    }
+    constructor(int num){
+        System.out.println("parameterized constructor called");
+    }
+
+    public static void main(String args[]){
+        constructor obj1 = new constructor();
+        constructor obj2 = new constructor(10);
+    }
+}
---
+++ modulus.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class modulus {
+    public static void main(String args[]){
+        int num1,num2;
+        System.out.println("enter two numbers: ");
+        Scanner scan = new Scanner(System.in);
+        num1 = scan.nextInt();
+        num2 = scan.nextInt();
+
+        System.out.printf("modulus: %d\n",(num1%num2));
+        scan.close();
+    }
+}
